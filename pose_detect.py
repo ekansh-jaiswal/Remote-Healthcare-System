77232a14011891b5fa91c237444a439a1ec08020
Ekansh Jaiswal <83162629+ekansh-jaiswal@users.noreply.github.com>
2026-05-13 01:07:00 +0530

    Commit

--- pose_detect.py
+++ pose_detect.py
@@ -12,10 +12,8 @@ import start
 
 mp_pose = mp.solutions.pose
 
-# Setting up the Pose function.
 pose = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.3, model_complexity=2)
 
-# Initializing mediapipe drawing class, useful for annotation.
 mp_drawing = mp.solutions.drawing_utils 
 
 def sample_pose_detect_img(path):
@@ -297,50 +295,32 @@ def detectPose(image, pose, display=True):
         landmarks: A list of detected landmarks converted into their original scale.
     '''
     
-    # Create a copy of the input image.
     output_image = image.copy()
     
-    # Convert the image from BGR into RGB format.
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    # Perform the Pose Detection.
     results = pose.process(imageRGB)
     
-    # Retrieve the height and width of the input image.
     height, width, _ = image.shape
     
-    # Initialize a list to store the detected landmarks.
     landmarks = []
-    
-    # Check if any landmarks are detected.
     if results.pose_landmarks:
     
-        # Draw Pose landmarks on the output image.
         mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
                                   connections=mp_pose.POSE_CONNECTIONS)
         
-        # Iterate over the detected landmarks.
         for landmark in results.pose_landmarks.landmark:
             
-            # Append the landmark into the list.
             landmarks.append((int(landmark.x * width), int(landmark.y * height),
                                   (landmark.z * width)))
     
-    # Check if the original input image and the resultant image are specified to be displayed.
     if display:
-    
-        # Display the original input image and the resultant image.
         plt.figure(figsize=[22,22])
         plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
         plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
-        
-        # Also Plot the Pose landmarks in 3D.
         mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
         
-    # Otherwise
     else:
-        
-        # Return the output image and the found landmarks.
         return output_image, landmarks
 
 def detectPose_for_image(image, pose, display=True):
@@ -356,51 +336,38 @@ def detectPose_for_image(image, pose, display=True):
         landmarks: A list of detected landmarks converted into their original scale.
     '''
     
-    # Create a copy of the input image.
     output_image = image.copy()
     
-    # Convert the image from BGR into RGB format.
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
-    # Perform the Pose Detection.
     results = pose.process(imageRGB)
     
-    # Retrieve the height and width of the input image.
     height, width, _ = image.shape
     
-    # Initialize a list to store the detected landmarks.
     landmarks = []
     
-    # Check if any landmarks are detected.
     if results.pose_landmarks:
     
-        # Draw Pose landmarks on the output image.
         mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
                                   connections=mp_pose.POSE_CONNECTIONS)
         
-        # Iterate over the detected landmarks.
         for landmark in results.pose_landmarks.landmark:
             
-            # Append the landmark into the list.
             landmarks.append((int(landmark.x * width), int(landmark.y * height),
                                   (landmark.z * width)))
     
-    # Check if the original input image and the resultant image are specified to be displayed.
     if display:
     
-        # Display the original input image and the resultant image.
         plt.figure(figsize=[22,22])
         plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
         plt.subplot(122);plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
         
-        # Also Plot the Pose landmarks in 3D.
         mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
         start.home_page()
         
     # Otherwise
     else:
         
-        # Return the output image and the found landmarks.
         return output_image, landmarks
         start.home_page()
 
@@ -416,21 +383,16 @@ def calculateAngle(landmark1, landmark2, landmark3):
 
     '''
 
-    # Get the required landmarks coordinates.
     x1, y1, _ = landmark1
     x2, y2, _ = landmark2
     x3, y3, _ = landmark3
 
-    # Calculate the angle between the three points
     angle = math.degrees(math.atan2(y3 - y2, x3 - x2) - math.atan2(y1 - y2, x1 - x2))
     
-    # Check if the angle is less than zero.
     if angle < 0:
 
-        # Add 360 to the found angle.
         angle += 360
     
-    # Return the calculated angle.
     return angle
 
 def classifyPose(landmarks, output_image, display=False):
@@ -448,10 +410,8 @@ def classifyPose(landmarks, output_image, display=False):
     '''
     log = open('log.txt', 'a+')
     
-    # Initialize the label of the pose. It is not known at this stage.
     label = 'Unknown Pose'
 
-    # Specify the color (Red) with which the label will be written on the image.
     color = (0, 0, 255)
     
     # Calculate the required angles.
@@ -579,42 +539,30 @@ def classifyPose(landmarks, output_image, display=False):
         
     else:
         
-        # Return the output image and the classified label.
         return output_image, label
 
 def live_vid_and_pose():
     # Setup Pose function for video.
     pose_video = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, model_complexity=1)
 
-    # Initialize the VideoCapture object to read from the webcam.
     camera_video = cv2.VideoCapture(0)
     camera_video.set(3,1280)
     camera_video.set(4,960)
 
-    # Initialize a resizable window.
     cv2.namedWindow('Pose Classification', cv2.WINDOW_NORMAL)
 
-    # Iterate until the webcam is accessed successfully.
     while camera_video.isOpened():
-        # Read a frame.
         ok, frame = camera_video.read()
     
-        # Check if frame is not read properly.
         if not ok:
-        
-            # Continue to the next iteration to read the next frame and ignore the empty camera frame.
             continue
     
-        # Flip the frame horizontally for natural (selfie-view) visualization.
         frame = cv2.flip(frame, 1)
     
-        # Get the width and height of the frame
         frame_height, frame_width, _ =  frame.shape
     
-        # Resize the frame while keeping the aspect ratio.
         frame = cv2.resize(frame, (int(frame_width * (640 / frame_height)), 640))
     
-        # Perform Pose landmark detection.
         frame, landmarks = detectPose(frame, pose_video, display=False)
     
         # Check if the landmarks are detected.
